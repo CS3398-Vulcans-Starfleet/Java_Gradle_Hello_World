0d258f2ea51218bc32cb620b12fc88a3b1d6c60a
Mason C <43713560+mercurrie@users.noreply.github.com>
2020-03-02 19:38:45 -0600

    add comment

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,9 +2,17 @@ package hello;
 /* Get a time library */
 import org.joda.time.LocalTime;
 
+<<<<<<< Updated upstream
 public class HelloWorld
 {
 	/* test travis-ci */
+=======
+//I'm making a comment
+public class HelloWorld
+{
+	/* master comment */
+  // Mason's comment
+>>>>>>> Stashed changes
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
